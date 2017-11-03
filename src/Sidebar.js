44d23f69027b6bb47f99614c8ed918f1cfe32360
Elsha Kwee <elsha.e.k@gmail.com>
2017-11-03 15:46:27 +0700

    ADD scene
    Sidebar.js

---
+++ src/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Header, Content, Button, Text, Icon } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <Container>
+      <Header/>
+      <Content>
+        <Button
+          transparent
+          dark
+          onPress={ () => Actions.push("profile")}
+        >
+          <Icon name="person" />
+          <Text>Profile</Text>
+        </Button>
+      </Content>
+      </Container>
+    );
+  }
+}
+
+export default Sidebar;
